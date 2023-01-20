a7dbd33c0ef204d5c88d4ee9e92c728c744e47e2
Lywën <laurie@alarcon.eu>
2023-01-20 16:43:24 +0100

    ajout du bouton retour dans task_detail

--- todoproject/todolist/urls.py
+++ todoproject/todolist/urls.py
@@ -3,6 +3,6 @@ from todolist import views
 
 urlpatterns = [
     path ('about/', views.AboutView.as_view()),
-    path('tasks/', views.TasksView.as_view()),
+    path('tasks/', views.TasksView.as_view(), name="tasks_list"),
     path('tasks/<int:task_id>', views.TaskDetailView.as_view(), name="task_detail")
 ]
